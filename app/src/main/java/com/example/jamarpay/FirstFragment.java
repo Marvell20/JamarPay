9f4923574c3043f2c330610de899de8f69a07a38
josueaorozco <113638774+josueaorozco@users.noreply.github.com>
2023-08-02 16:45:40 -0500

    fix/new_design

--- app/src/main/java/com/example/jamarpay/FirstFragment.java
+++ app/src/main/java/com/example/jamarpay/FirstFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,6 +50,7 @@ public class FirstFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
